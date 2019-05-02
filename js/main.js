43995a64173ff98ff08aa776c04d067f5364b914
Liban Osman <32774298+LosmanDev@users.noreply.github.com>
2019-05-02 19:15:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let title = document.querySelector(".title");
 let firstName = document.querySelector(".firstName");
 let lastName = document.querySelector(".lastName");
 let age = document.querySelector(".age");
+let loc = document.querySelector(".location");
 
 function getPerson() {
   fetch("https://randomuser.me/api/")
@@ -19,6 +20,8 @@ function getPerson() {
       firstName.textContent = data.results[0].name.first.toUpperCase();
       lastName.textContent = data.results[0].name.last.toUpperCase();
       age.textContent = data.results[0].dob.age;
+      loc.innerHTML = `City - ${data.results[0].location.state.toUpperCase()} <br/>
+      State - ${data.results[0].location.city.toUpperCase()}`;
 
       if (data.results[0].gender === "female") {
         gender.innerHTML = "Female" + ' <i class="fas fa-female"></i>';
